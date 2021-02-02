28e049079597bae83a13debf7f453df931fa370e
hz6 <hz6@ualberta.ca>
2021-02-02 17:34:47 +0800

    set state hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
+  let [posts, setPosts] = useState([]);
+  let [loading, setLoading] = useState(false);
+  let [currentPage, setCurrentPage] = useState(1);
+  let [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    }
+  })
 
   return (
     <div className="App">
